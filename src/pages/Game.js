562e4cb0832721cfd4e5ce7ceadfb8b8d6362e45
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-02-09 19:30:57 -0300

    requisito 05

---
+++ src/pages/Game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Página do Game</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
