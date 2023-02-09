5bb99bde1dc26629bb99e7d4690e5480a7798761
mthsmelo <matheusaguiarmelo@gmail.com>
2023-02-09 19:57:08 -0300

    requisito 13 e 14 (não passando nos testes)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
+import Feedback from './pages/Feedback';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/feedback" component={ Feedback } />
     </Switch>
   );
 }
