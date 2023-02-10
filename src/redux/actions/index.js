0ec02ad4adf732ea20d75ebc76400474455cec8b
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-02-10 16:37:12 -0300

    requisito 9

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,9 +2,15 @@ import md5 from 'crypto-js/md5';
 
 const URL_BASE = 'https://www.gravatar.com/avatar/';
 export const SAVE_AVATAR = 'SAVE_AVATAR';
+export const SAVE_SCORE = 'SAVE_SCORE';
 
 export const ACTION_PLAYER = () => ({ type: '',
   payload,
+});
+
+export const ACTION_SCORE = (points) => ({
+  type: SAVE_SCORE,
+  payload: points,
 });
 
 export const ACTION_AVATAR = (email, name) => {
