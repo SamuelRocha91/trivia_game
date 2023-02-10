1a681b46b4b11aeaf41554e6a3044dda34e415d2
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-02-10 17:42:36 -0300

    requisito 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
@@ -8,6 +9,7 @@ class Feedback extends Component {
     const three = 3;
     return (
       <div>
+        <Header />
         {assertions < three
           ? <p data-testid="feedback-text">Could be better...</p>
           : <p data-testid="feedback-text">Well Done!</p>}
